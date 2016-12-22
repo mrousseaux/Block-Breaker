2c78c9b6876e726fc1b213aa0b70f5064c5e5d21
mrousseaux <matthew@rousseaux.ca>
2016-12-21 22:30:44 -0600

    Added block color change
    Added block color change on hit

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -9,8 +9,10 @@ public class Brick : MonoBehaviour {
 	void OnCollisionEnter2D (Collision2D brickCollision){
 		brickHits++;
 		//Debug.Log ("Brick: " + GetInstanceID() + " /Hits:" + brickHits);
-		SpriteRenderer brickColor = gameObject.GetComponent( typeof(SpriteRenderer) ) as SpriteRenderer;//Get the renderer via GetComponent or have it cached previously
-		brickColor.color = new Color(0f, 0f, 0f, 1f); // Set to opaque black
+
+		SpriteRenderer brickColor = gameObject.GetComponent( typeof(SpriteRenderer) ) as SpriteRenderer;//Get the renderer via GetComponent
+		brickColor.color = new Color(brickColor.color.r,brickColor.color.g,brickColor.color.b, 0.5f); // Set to half it's alpha
+
 		if (brickHits == maxHits){
 			Destroy(gameObject);
 			Debug.Log ("Brick Destroyed");
