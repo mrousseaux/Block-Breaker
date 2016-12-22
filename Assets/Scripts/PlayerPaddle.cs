d6aac9e8c51c822a45da05f31f3d61ab71fdb30b
mrousseaux <matthew@rousseaux.ca>
2016-12-21 20:25:27 -0600

    Player paddle movement
    Added movement to the player paddle and mathf.clamp to stay within the
    playspace

--- Assets/Scripts/PlayerPaddle.cs
+++ Assets/Scripts/PlayerPaddle.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class PlayerPaddle : MonoBehaviour {
 
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
 	// Update is called once per frame
 	void Update () {
-	
+		float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+
+		Vector3 paddlePos = new Vector3 (mousePosInBlocks, this.transform.position.y, this.transform.position.z);
+
+		paddlePos.x = Mathf.Clamp (mousePosInBlocks,0.5f,15.5f);
+
+		this.transform.position = paddlePos;
+
 	}
+	
 }
