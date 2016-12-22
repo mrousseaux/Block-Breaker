d0bf5fc43e8dd8de0a601c96c3dcab0cd812612b
mrousseaux <matthew@rousseaux.ca>
2016-12-21 19:20:07 -0600

    Physics
    ball and paddle have physics and colliders to interact. Ball bounces off
    the paddle. (MORE than 100% of the energy is being retained though....
    not sure about that)

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class LoseCollider : MonoBehaviour {
+	public LevelManager levelManager;
+	
+	void OnTriggerEnter2D (Collider2D loseTrigger){
+		Debug.Log ("Lose Trigger Detected.");
+		levelManager.LoadLevel ("Lose Screen");
+	}
+	//void OnCollisionEnter2D (Collision2D loseCollider){
+	//	Debug.Log ("Lose Collision Detected.");
+	//}
+}
