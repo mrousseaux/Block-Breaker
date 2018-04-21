c88af2daabdf00dea68e514002e126b710714083
Matthew Rousseaux <matthew@rousseaux.ca>
2018-04-20 20:45:25 -0600

    Sprites
    added sprites to the blocks, adjusted hit detection to account for sprites.

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -2,25 +2,42 @@
 using System.Collections;
 
 public class Brick : MonoBehaviour {
-	public int maxHits = 1;
+	public Sprite[] hitSprites;
+
 	private LevelManager levelManager;
 	private int brickHits = 0;
 	
 
 	void OnCollisionEnter2D (Collision2D brickCollision){
-		brickHits++;
-		//Debug.Log ("Brick: " + GetInstanceID() + " /Hits:" + brickHits);
+		if (gameObject.tag == "Unbreakable") {
+		} else {
+			int maxHits = hitSprites.Length + 1;
+			brickHits++;
+			//Debug.Log ("Brick: " + GetInstanceID() + " /Hits:" + brickHits);
 
-		SpriteRenderer brickColor = gameObject.GetComponent( typeof(SpriteRenderer) ) as SpriteRenderer;//Get the renderer via GetComponent
-		brickColor.color = new Color(brickColor.color.r,brickColor.color.g,brickColor.color.b, 0.5f); // Set to half it's alpha
+			SpriteRenderer brickColor = gameObject.GetComponent (typeof(SpriteRenderer)) as SpriteRenderer;//Get the renderer via GetComponent
+			brickColor.color = new Color (brickColor.color.r, brickColor.color.g, brickColor.color.b, 0.5f); // Set to half it's alpha
 
-		if (brickHits >= maxHits){
-			Destroy(gameObject);
-			Debug.Log ("Brick Destroyed");
-			SimulateWin();
+			if (brickHits >= maxHits) {
+				Destroy (gameObject);
+				Debug.Log ("Brick Destroyed");
+				//SimulateWin ();
+			} else {
+				LoadSprites ();
+			}
 		}
 
 	}
+
+	void LoadSprites(){
+		int sprintIndex = brickHits - 1;
+		if (hitSprites [sprintIndex]) {
+			this.GetComponent<SpriteRenderer> ().sprite = hitSprites [sprintIndex];
+		} else {
+			Debug.Log ("Block is missing a Hit Sprit at index: " + sprintIndex);
+		}
+	}
+
 
 	// TODO Remove this method with real win conditions
 	void SimulateWin(){
